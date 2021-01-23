5e735a7a72158097a36f135216d5b2f41d4c10c1
maximus986 <aleksandar.maksimovic2608@gmail.com>
2021-01-23 23:33:17 +0100

    Add useful links page

--- src/pages/korisni-linkovi.js
+++ src/pages/korisni-linkovi.js
@@ -1,18 +1,117 @@
 /** @jsx jsx */
-import { usePageContext, useTranslation } from '@3nvi/gatsby-theme-intl';
-import { jsx } from 'theme-ui';
+import React from 'react';
+import { useTranslation } from '@3nvi/gatsby-theme-intl';
+import { Grid, jsx } from 'theme-ui';
 import SEO from 'components/seo';
+import { useLocalizedWpData } from 'hooks';
+import { PageIntro, Container } from 'components';
 
-const Links = () => {
+const Links = ({ data }) => {
   const { t } = useTranslation();
-  const { lang } = usePageContext();
-
+  const localizedPageData = useLocalizedWpData(
+    data.allWpPage.nodes.filter(({ usefulLinksPage }) => {
+      if (usefulLinksPage) {
+        return usefulLinksPage.webinars;
+      }
+    }),
+  )[0];
+  const {
+    usefulLinksPage: {
+      pageIntros: {
+        pageIntroItem: { pageTitle, pageSubtitle, pageIntroImage },
+      },
+      webinars,
+    },
+  } = localizedPageData;
   return (
-    <div sx={{ bg: 'white', padding: 4 }}>
+    <>
       <SEO title={t('links')} />
-      <h1>{t('links')}</h1>
-    </div>
+      <PageIntro
+        pageTitle={pageTitle}
+        pageSubtitle={pageSubtitle}
+        pageIntroImage={pageIntroImage}
+      />
+      <section sx={{ py: [8] }}>
+        <Container>
+          <Grid columns={[null, null, null, 2]} gap={8}>
+            {webinars.map(({ webinar }, id) => (
+              <>
+                <iframe
+                  src={webinar.webinarItem.webinarVideoLink}
+                  title={webinar.webinarItem.webinarTitle}
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  frameBorder="0"
+                  webkitallowfullscreen="true"
+                  mozallowfullscreen="true"
+                  allowFullScreen
+                  width="100%"
+                  height="400"
+                  loading="lazy"
+                  key={id}
+                />
+                <iframe
+                  src={webinar.webinarItem.webinarVideoLink}
+                  title={webinar.webinarItem.webinarTitle}
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  frameBorder="0"
+                  webkitallowfullscreen="true"
+                  mozallowfullscreen="true"
+                  allowFullScreen
+                  width="100%"
+                  height="400"
+                  loading="lazy"
+                  key={id}
+                />
+              </>
+            ))}
+          </Grid>
+        </Container>
+      </section>
+    </>
   );
 };
 
 export default Links;
+
+export const PAGE_QUERY = graphql`
+  {
+    allWpPage {
+      nodes {
+        language {
+          code
+        }
+        usefulLinksPage {
+          pageIntros {
+            ... on WpPageIntro {
+              id
+              pageIntroItem {
+                pageSubtitle
+                pageTitle
+                pageIntroImage {
+                  localFile {
+                    childImageSharp {
+                      fluid(quality: 100, toFormat: WEBP) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          webinars {
+            webinar {
+              ... on WpWebinar {
+                id
+                webinarItem {
+                  webinarTitle
+                  webinarVideoLink
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
