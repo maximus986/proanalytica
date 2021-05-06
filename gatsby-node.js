cf9bbc19b6933860db30893e87cbcdc56cc0e42f
maximus986 <aleksandar.maksimovic2608@gmail.com>
2021-05-06 21:29:24 +0200

    404 page redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,3 +23,18 @@ exports.createPages = async ({ actions, graphql }) => {
   await createPostsPages({ actions, graphql });
   await createProductsListPages({ actions, graphql });
 };
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage, deletePage } = actions;
+  // Check if the page is a localized 404
+  if (page.path.match(/^\/[a-z]{2}\/404\/$/)) {
+    const oldPage = { ...page };
+    // Get the language code from the path, and match all paths
+    // starting with this code (apart from other valid paths)
+    const langCode = page.path.split(`/`)[1];
+    page.matchPath = `/${langCode}/*`;
+    // Recreate the modified page
+    deletePage(oldPage);
+    createPage(page);
+  }
+};
