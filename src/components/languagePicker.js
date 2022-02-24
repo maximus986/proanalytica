bb40f129983488639576cc76353d4580a287a865
maximus986 <aleksandar.maksimovic2608@gmail.com>
2022-02-24 18:18:42 +0100

    Make user awar which language will select

--- src/components/languagePicker.js
+++ src/components/languagePicker.js
@@ -5,36 +5,72 @@ import { Link as GatsbyLink } from 'gatsby';
 import RSFlag from 'images/rs.png';
 import ENFlag from 'images/gb.png';
 import styled from '@emotion/styled';
+import { useTranslation } from '@3nvi/gatsby-theme-intl';
 
 export const LanguagePicker = () => {
   const { lang, originalPath } = usePageContext();
+  const { t } = useTranslation();
   const sr = lang === 'sr' ? 'cir' : 'sr';
   const isSerbian = lang === 'sr' || lang === 'cir';
 
   return (
     <Container>
-      <LangLink
-        aria-label={`Change language to SR`}
-        to={`/${sr}${originalPath}`}
-        sx={{ mr: 3, opacity: isSerbian ? 1 : 0.5 }}
+      <LangContainer
+        sx={{
+          mr: 3,
+          opacity: isSerbian ? 1 : 0.5,
+        }}
       >
-        <img src={RSFlag} alt="Serbian flag" />
-      </LangLink>
-      <LangLink
-        aria-label={`Change language to EN`}
-        to={`/en${originalPath}`}
-        sx={{ opacity: !isSerbian ? 1 : 0.5 }}
+        <LangLink
+          aria-label={`Change language to SR`}
+          to={`/${sr}${originalPath}`}
+        >
+          <img src={RSFlag} alt="Serbian flag" />
+        </LangLink>
+        <LangName>{`${t(sr)}`}</LangName>
+      </LangContainer>
+      <LangContainer
+        sx={{
+          opacity: !isSerbian ? 1 : 0.5,
+        }}
       >
-        <img src={ENFlag} alt="Serbian flag" />
-      </LangLink>
+        <LangLink
+          aria-label={`Change language to EN`}
+          to={`/en${originalPath}`}
+        >
+          <img src={ENFlag} alt="Serbian flag" />
+        </LangLink>
+        <LangName>{t('en')}</LangName>
+      </LangContainer>
     </Container>
   );
 };
 
+const LangName = ({ children }) => {
+  return (
+    <span
+      sx={{
+        color: 'primary',
+        m: 0,
+        lineHeight: 1,
+        mt: '3px',
+        fontSize: 0,
+      }}
+    >
+      {children}
+    </span>
+  );
+};
+
 const Container = styled.div`
-  height: 20px;
   display: flex;
   align-items: center;
+`;
+
+const LangContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const LangLink = styled(GatsbyLink)`
